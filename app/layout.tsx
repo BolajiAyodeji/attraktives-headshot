a3249b5ff2e5579d93aeba95f8d8c2f6dcf630c3
Bolaji Ayodeji <sirbeejay1@gmail.com>
2024-04-18 10:32:28 +0100

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { IBM_Plex_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
@@ -60,7 +61,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={ibm.className}>{children}</body>
+        <body className={ibm.className}>
+          {children}
+          <Analytics />
+        </body>
       </html>
     </ClerkProvider>
   );
